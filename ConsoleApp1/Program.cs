77f0fe85ab91e736339d79bc3d65369e24a064bb
Student <Student@LabVM2047644>
2026-08-12 17:02:29 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿//namespace
+using Microsoft.VisualBasic;
+
+namespace Programming;
+
+// class
+public class Programming()
+{
+
+ //main method
+    static void Main(string[] args);
+
+
+string name = David;
+int age = 12
+system.console.Writeline("name");
+
+system.console.Writeline("age");
+
+}
+// operators
+
+
+
+
